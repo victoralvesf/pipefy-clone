38c8b25ed6cd42b75474bf928fe9ad52f3e8d454
l0rddarkside <viictor.alvess12@gmail.com>
2019-08-22 21:20:19 -0300

    borda nas listas

--- src/components/Board/styles.js
+++ src/components/Board/styles.js
@@ -4,4 +4,7 @@ export const Container = styled.div`
   display: flex;
   padding: 30px 0;
   height: calc(100% - 80px);
+  & div {
+    border-left: 0.5px solid rgba(0,0,0,0.05);
+  }
 `;
--- src/components/List/styles.js
+++ src/components/List/styles.js
@@ -6,10 +6,6 @@ export const Container = styled.div`
   flex: 0 0 320px;
   opacity: ${props => props.done ? 0.6 : 1}
 
-  & + div {
-      border-left: 1px solid rgba(0,0,0,0.05);
-  }
-
   header {
       display: flex;
       justify-content: space-between;
